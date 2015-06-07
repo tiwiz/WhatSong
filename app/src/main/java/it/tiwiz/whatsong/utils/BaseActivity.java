d17c1ccdcddc633d99691f2f196f96a0aac14cdf
Roberto Orgiu <roberto.orgiu@gmail.com>
2015-06-07 23:39:45 +0200

    Adds signature for package

--- app/src/main/java/it/tiwiz/whatsong/utils/BaseActivity.java
+++ app/src/main/java/it/tiwiz/whatsong/utils/BaseActivity.java
@@ -2,7 +2,6 @@ package it.tiwiz.whatsong.utils;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
